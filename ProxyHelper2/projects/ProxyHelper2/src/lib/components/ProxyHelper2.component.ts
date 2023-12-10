13ce034a92ec3d545e7cad0df7733062ba6d61f6
hoodoer <drew.kirkpatrick@gmail.com>
2023-12-10 17:21:04 -0500

    Got base UI layout working

--- ProxyHelper2/projects/ProxyHelper2/src/lib/components/ProxyHelper2.component.ts
+++ ProxyHelper2/projects/ProxyHelper2/src/lib/components/ProxyHelper2.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectionList } from '@angular/material/list';
 // import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'lib-ProxyHelper2',
     templateUrl: './ProxyHelper2.component.html',
-    styleUrls: ['./ProxyHelper2.component.css']
+    styleUrls: ['./ProxyHelper2.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 
 
 export class ProxyHelper2Component implements OnInit {
     constructor(private API: ApiService) { }
+    @ViewChild('portForwardList') portForwardList: MatSelectionList;
 
-    isChecked = false;
-    apiResponse = 'Press the button to get response.';
-    // ipForm: FormGroup;
+    isChecked      = false;
+    apiResponse    = 'Press the button to get response.';
+    proxyIpAddress = '';
+    proxyPort      = '';
+
+    selectedPorts: string[] = ['80', '443'];
+
+    ports = [
+        { value: '80', checked: false },
+        { value: '443', checked: true },
+        ];
+
 
     doButtonPress(): void {
         this.API.request({
@@ -29,6 +42,8 @@ export class ProxyHelper2Component implements OnInit {
     routingToggle(): void {
         console.log("Routing toggle changed: " + this.isChecked);
 
+        console.log("About to proxy route: " + this.proxyIpAddress + ': ' + this.proxyPort);
+
         this.API.request({
             module: 'ProxyHelper2',
             action: 'routingToggle',
@@ -40,7 +55,9 @@ export class ProxyHelper2Component implements OnInit {
     }
 
 
-
+    getSelectedPorts(): string[] {
+        return this.ports.filter(port => port.checked).map(port => port.value);
+    }
 
 
     ngOnInit() {
